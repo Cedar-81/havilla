693ab4a581a4abae0087d2149f681ddf67a92e5e
Cedar-81 <64336732+Cedar-81@users.noreply.github.com>
2024-01-31 17:11:00 -1100

    changed sveltkit config

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      assets: '/public' // Set your custom output directory here
+    }
   },
   preprocess: vitePreprocess()
 };
+
 export default config;
