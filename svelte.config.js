13bf4dd2062cb7d03fbf0bba7f77ff6874315b06
Cedar-81 <64336732+Cedar-81@users.noreply.github.com>
2024-01-31 17:19:16 -1100

    changed config

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
-// svelte.config.js
-import adapter from "@sveltejs/adapter-static"
-import { vitePreprocess } from "@sveltejs/kit/vite"
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import("@sveltejs/kit").Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
-    preprocess: vitePreprocess(),
-
-    kit: {
-        adapter: adapter({ pages: "public" })
-    }
-}
-
-export default config
+  kit: {
+    adapter: adapter(pages: "public")
+  },
+  preprocess: vitePreprocess()
+};
+export default config;
